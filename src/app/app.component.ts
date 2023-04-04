9f1e6eb5ec573187b505bfa51634270975e896a5
Anthony Adams <ant@anthonys-mbp.lan>
2023-04-03 19:49:29 -0500

    day2changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'antsdetailing';
+  
+  message = "I'm read only!";
+  canEdit = false;
+ constructor(){
+  this.message = 'message after construction';
+ }
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = "I'm read only!";
+    }
+  }
 }
